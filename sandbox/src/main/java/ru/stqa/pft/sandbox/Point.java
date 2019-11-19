b5722d9b74433538ce59bfe8595ebf61fa55a6c6
Slava <narbeckov.slava@yandex.ru>
2019-11-19 22:35:27 +0300

    Secondary part of task for lesson 2
    4. Реализовать то же самое (вычисление расстояния между двумя точками) при помощи метода в классе Point, и добавить в созданный в предыдущем пункте запускаемый класс примеры использования метода вместо ранее созданной функции.

--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -11,4 +11,9 @@ public class Point {
         this.y = y;
     }
 
+    public double distance(Point p2)
+    {
+        return Math.sqrt(((p2.x-this.x)*(p2.x-this.x)+((p2.y-this.y)*(p2.y-this.y))));
+    }
+
 }
